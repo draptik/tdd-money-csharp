f3f1ef4b37b838536c88724fb495db6c6db2a478
Patrick Drechsler <patrick@pdrechsler.de>
2020-02-22 21:12:56 +0100

    ch1 - compile, test still fails

---
+++ Dollar.cs
@@ -0,0 +1,15 @@
+namespace TDDMoney.Tests
+{
+    public class Dollar
+    {
+        public Dollar(int amount)
+        {
+        }
+        
+        public int Amount { get; }
+
+        public void Times(int multiplier)
+        {
+        }
+    }
+}
