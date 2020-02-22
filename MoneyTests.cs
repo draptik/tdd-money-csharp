0b2db8efd9aea3345ed59e6a4031a4f250252ead
Patrick Drechsler <patrick@pdrechsler.de>
2020-02-22 22:14:00 +0100

    ch6 - add missing equality checks for Francs

--- MoneyTests.cs
+++ MoneyTests.cs
@@ -26,6 +26,8 @@ namespace TDDMoney.Tests
         {
             new Dollar(5).Should().Be(new Dollar(5));
             new Dollar(5).Should().NotBe(new Dollar(6));
+            new Franc(5).Should().Be(new Franc(5));
+            new Franc(5).Should().NotBe(new Franc(6));
         }
         
         
