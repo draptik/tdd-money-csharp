28fd74a8095a15bc5562219eecb3e3b90f8f2d1f
Patrick Drechsler <patrick@pdrechsler.de>
2020-02-23 00:46:18 +0100

    ch10 - create failing test for class comparison

--- MoneyTests.cs
+++ MoneyTests.cs
@@ -37,5 +37,12 @@ namespace TDDMoney.Tests
             Money.Dollar(1).Currency.Should().Be("USD");
             Money.Franc(1).Currency.Should().Be("CHF");
         }
+
+        [Fact]
+        public void Different_class_equality()
+        {
+            new Money(10, "CHF")
+                .Should().Be(new Franc(10, "CHF"));
+        }
     }
 }
